4d319bcd4d9da480e2ce3da3f7d5fd9a1cbea72e
zhonxinya <15205182930@163.com>
2026-05-13 12:00:53 +0800

    chore: 删除 qidianbook 和 xunleipan 插件

--- plugins.v2/qidianbook/core/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-核心服务层模块
-"""
-
-from .client import QidianClient
-from .search_service import SearchService
-from .auth_service import AuthService
-
-__all__ = ['QidianClient', 'SearchService', 'AuthService']
--- plugins.v2/xunleipan/dist/assets/__federation_fn_import-7dad6dfa.js
+++
@@ -1,1 +0,0 @@
-export { i as importShared, a as importSharedLocal, g as importSharedRuntime } from './vendor-fec5d75f.js';
